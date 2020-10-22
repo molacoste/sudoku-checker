a93745543755b42058dc5102af8dbc478379a800
Marc-Olivier Lacoste <marc-olivier.lacoste@usherbrooke.ca>
2020-10-22 12:23:19 -0400

    Add dispatch logic for verification
    All is left is 1. intra-line/column/block verification 2. Error message sub-program

--- sudokuChecker.s
+++ sudokuChecker.s
@@ -1,6 +1,6 @@
 /********************************************************************************
-*	Programme qui lit, affiche et vérifie un sudoku.                       	*
-*	Auteur: Marc-Olivier Lacoste            				*
+*	Programme qui lit, affiche et vérifie un sudoku.                         	*
+*	Auteur: Marc-Olivier Lacoste            									*
 ********************************************************************************/
 
 .include "/root/SOURCES/ift209/tools/ift209.as"
@@ -10,25 +10,26 @@
 
 NBLIGNES = 9
 NBCOLS = 9
+NBBLOCS = 9
 
 /* Début du programme */
 Main:
 		adr		x20, Sudoku			//x20 contient l'adresse de base du sudoku
 
-        	mov		x0, x20				//Paramètre: adresse du sudoku
-        	bl      	LireSudoku			//Appelle le sous-programme de lecture
+       	mov		x0, x20				//Paramètre: adresse du sudoku
+        bl     	LireSudoku			//Appelle le sous-programme de lecture
 
 		mov		x0, x20				//Paramètre: adresse du sudoku
-		bl		AfficherSudoku			//Appelle le sous-programme d'affichage
+		bl		AfficherSudoku		//Appelle le sous-programme d'affichage
 
 		mov		x0, x20				//Paramètre: adresse du sudoku
-		bl		VerifierSudoku			//Appelle le sous-programme de vérification
+		bl		VerifierSudoku		//Appelle le sous-programme de vérification
 
 		mov		x0, #0				//0: tous les tampons
 		bl		fflush				//Vidange des tampons
 
 		mov		x0, #0				//0: aucune erreur
-		bl      	exit				//Fin du programme
+		bl     	exit				//Fin du programme
 
 /********************************************************************************
 *  Sous-programme qui lit le sudoku en vecteur de taille 81 entiers             *
@@ -39,37 +40,38 @@ LireSudoku:
 
 		mov		x19, x0				// Conserve l'adresse du sudoku dans x19
 
-		mov		x27, NBLIGNES			// x27 = # lignes
+		mov		x27, NBLIGNES		// x27 = # lignes
 		mov		x28, NBCOLS			// x28 = # colonnes
 
-		mul		x20, x27, x28			// Nombre d'éléments = lignes * colonnes (81)
+		mul		x20, x27, x28		// Nombre d'éléments = lignes * colonnes (81)
 		mov		x21, 0				// Index dans la matrice
 
 LireSudoku10:
 		adr		x0, scfmt1			// Param1: format de lecture
-		add		x1, x19, x21			// Param2: adresse de l'element a lire
+		add		x1, x19, x21		// Param2: adresse de l'element a lire
 		bl		scanf				// Lit l'element courant
 
 		add		x21, x21, 4			// Avance l'index d'un element (4 octets)
-		subs		x20, x20, 1			// Decremente le nombre d'ekements a lire
-		b.gt		LireSudoku10			// Si le nombre est > 0, alors on lit encore
+		subs	x20, x20, 1			// Decremente le nombre d'ekements a lire
+		b.gt	LireSudoku10		// Si le nombre est > 0, alors on lit encore
 
 		RESTORE						// Ramene l'environnment de l'appelant
-		br		x30				// Retour a l'appelant
+		br		x30					// Retour a l'appelant
+
 
 /********************************************************************************
-*  Sous-programme qui affiche le sudoku colonne par colonne		        *
-*										*
-*  i = no de ligne, j = no de colonne						*
-*  Position de l'element courant: adr.base + [i * (nb.colonnes)+j]*4		*
-*										*
+*  Sous-programme qui affiche le sudoku colonne par colonne		  			    *
+*																				*
+*  i = no de ligne, j = no de colonne											*
+*  Position de l'element courant: adr.base + [i * (nb.colonnes)+j]*4			*
+*																				*
 *  x0: addresse du sudoku                                                       *
 ********************************************************************************/
 AfficherSudoku:
 		SAVE						// Sauvegarde l'environnement de l'appelant
 
 		mov		x19, x0				// Conserve l'adresse du sudoku dans x19
-		mov		x27, NBLIGNES			// x27 = # lignes
+		mov		x27, NBLIGNES		// x27 = # lignes
 		mov		x28, NBCOLS			// x28 = # colonnes
 		mov		x23, 4				// Constante 4 dans x23
 		mov		x21, 0				// Compteur j = 0
@@ -78,9 +80,9 @@ AfficherSudoku:
 AfficherSudoku05:
 
 		mov		x26, 3
-		udiv		x25, x21, x26
+		udiv	x25, x21, x26
 		mul		x26, x26, x25
-		subs		x25, x21, x26
+		subs	x25, x21, x26
 		cbz		x25, AfficherSudokuSplitter
 
 		/* Boucle externe: controle le no de colonne (x21 = j) */
@@ -90,24 +92,24 @@ AfficherSudoku10:
 		/* S'occupe d'afficher les barres verte pour serparer les blocs */
 AfficherSudoku15:
 		mov		x26, 3
-		udiv		x25, x20, x26
+		udiv	x25, x20, x26
 		mul		x26, x26, x25
-		subs		x25, x20, x26
+		subs	x25, x20, x26
 		cbz		x25, AfficherSudokuBarreVert
 
 		/* Boucle interne: controle le no de ligne (x20 = i) */
 AfficherSudoku20:
-		mul		x22, x20, x28			// Debut de la rangee = i * nb.colonnes
-		add		x22, x22, x21			// Position de l'element = debut.rangee + j
-		mul 		x22, x22, x23			// Index = position.elmt * 4 octets par elmt
+		mul		x22, x20, x28		// Debut de la rangee = i * nb.colonnes
+		add		x22, x22, x21		// Position de l'element = debut.rangee + j
+		mul 	x22, x22, x23		// Index = position.elmt * 4 octets par elmt
 
 		adr		x0, ptfmt4			// Param1: adresse du format
-		ldr		w1, [x19,x22]			// Param2: valeur de l'element (base + index)
+		ldr		w1, [x19,x22]		// Param2: valeur de l'element (base + index)
 		bl		printf				// Affichage de l'element courant
 
 		add		x20, x20, 1			// Passe a la ligne suivante
 		cmp		x20, x27			// As-t-on depasse la derniere ligne?
-		b.lt		AfficherSudoku15		// Sinon, traite la ligne suivante
+		b.lt	AfficherSudoku15	// Sinon, traite la ligne suivante
 
 		/* Fin boucle interne */
 
@@ -117,46 +119,163 @@ AfficherSudoku20:
 
 		add		x21, x21, 1			// Passe a la colonne suivante
 		cmp		x21, x28			// As-t-on depasse la derniere colonne?
-		b.lt		AfficherSudoku05		// Sinon, traite la colonne suivante
+		b.lt	AfficherSudoku05	// Sinon, traite la colonne suivante
 
 		/* Affichage de la derniere barre verticale avec saut de ligne */
 		adr		x0, splitter
 		bl 		printf
 
-		b.al 		AfficherSudoku30
+		b.al 	AfficherSudoku30
 
 		/* Fin boucle externe */
 
 AfficherSudokuBarreVert:
 		adr		x0, barreVert
 		bl 		printf
-		b.al		AfficherSudoku20
+		b.al	AfficherSudoku20
 
 AfficherSudokuSplitter:
 		adr		x0, splitter
 		bl 		printf
-		b.al		AfficherSudoku10
+		b.al	AfficherSudoku10
 
 AfficherSudoku30:
 		RESTORE						//Ramène l'environnement de l'appelant
-		br		x30				//Retour à l'appelant
+		br		x30					//Retour à l'appelant
+
 
 /********************************************************************************
-*  Sous-programme qui verifie le sudoku en vecteur de taille 81 entiers         *
+*  Sous-programme qui "dispatch" les etapes de verification du sudoku	        *
 *  x0: addresse du sudoku                                                       *
 ********************************************************************************/
 VerifierSudoku:
 		SAVE						// Sauvegarde l'environnement de l'appelant
 
+		mov		x19, x0				// Conserve l'adresse du sudoku dans x19
+
+		mov		x0, x19				// Param1: adresse de base du sudoku
+		bl		VerifierR			// Branche au sous-programme de verification des rangees
+
+		mov		x0, x19				// Param1: adresse de base du sudoku
+		bl		VerifierC			// Branche au sous-programme de verification des colonnes
+
+		mov		x0, x19				// Param1: adresse de base du sudoku
+		bl		VerifierB			// Branche au sous-programme de verification des blocs
+
+VerifierR:
+		mov		x19, x0				// Conserve l'adresse du tableau dans x19
+		mov		x20, NBLIGNES		// Nombre de lignes a verifier
+
+VerifierRr:
+		cbz		x20, VerifierC		// Si on a fini d'evaluer les rangees, branche a VerifierC
+
+		mov		x0, x19				// Param1: l'adresse de l'element courant
+		bl		VerifierRangee		// Branche au sous-programme verifiant les rangees
+
+		sub		x20, x20, 1			// Decremente le nombre de rangees a verifier
+		add		x19, x19, 36		// Passe a l'adresse de base de la prochaine ligne (adr += 4*9)
+		b.al	VerifierRr			// Revient a la boucle pour prochaine evaluation
+
+VerifierC:
+		mov		x19, x0				// Conserve l'adresse du tableau dans x19
+		mov		x21, NBCOLS			// Nombre de colonnes a verifier
+
+VerifierCc:
+		cbz		x21, VerifierB		// Si on a fini d'evaluer les colonnes, branche a VerifierB
+
+		mov		x0, x19				// Param1: l'adresse de l'element courant
+		bl		VerifierColonne		// Branche au sous-programme verifiant les colonnes
+
+		sub		x21, x21, 1			// Decrement le nombre de colonnes a verifier
+		add		x19, x19, 4			// Passe l'adresse de base de la prochaine colonne (adr += 4)
+		b.al	VerifierCc			// Revient a la boucle pour prochaine evalutation
+
+VerifierB:
+		mov		x19, x0				// Conserve l'adresse du tableau dans x19
+		mov		x22, NBBLOCS		// Nombre de blocs a verifier
+
+VerifierBb:
+		cbz		x22, VerifierF		// Si on a fini d'evaluer les blocs, branche a VerifierF
+
+		mov		x0, x19				// Param1: l'adresse de l'element courant
+		bl		VerifierBloc		// Branche au sous-programme verifiant les blocs
+
+		sub		x22, x22, 1			// Decremente le nombre de blocs a verifier
+		add		x19, x19, 12		// Passe a l'adresse de base du prochain bloc (adr += 4*3)
+		b.al	VerifierBb			// Revient a la boucle pour prochaine evaluation
+
+VerifierF:
+		RESTORE
+		br		x30					//Retour à l'appelant
+
+/********************************************************************************
+*  Sous-programme qui verifie l'integralite des rangees du sudoku	            *
+*  x0: addresse du sudoku                                                       *
+********************************************************************************/
+VerifierRangee:
+		SAVE						// Sauvegarde l'environnement de l'appelant
+
+		mov		x19, x0				// Conserve l'adresse de base du sudoku
+		mov		x28, 9				// Nombre d'element dans une rangee a tenir compte
+		mov		x25, 0				// Bits pour verification init. a 0
+
+		/* faire allumer/etiendre des bits de x25 */
+
+		/*
+		* si all 1's -> la rangee est valide, passer a la prochaine
+		* sinon dispatch a sous-programme pour message d'erreur avec # de rangee
+		*/
 
 		RESTORE
-		br		x30				//Retour à l'appelant
+		br		x30					// Retour a l'appelant
+
+/********************************************************************************
+*  Sous-programme qui verifie l'integralite des colonnes du sudoku	            *
+*  x0: addresse du sudoku                                                       *
+********************************************************************************/
+VerifierColonne:
+		SAVE						// Sauvegarde l'environnement de l'appelant
+
+		mov		x19, x0				// Conserve l'adresse de base du sudoku
+		mov		x28, 9				// Nombre d'element dans une colonne a tenir compte
+		mov		x25, 0				// Bits pour verification init. a 0
+
+		/* faire allumer/etiendre des bits de x25 */
+
+		/*
+		* si all 1's -> la rangee est valide, passer a la prochaine
+		* sinon dispatch a sous-programme pour message d'erreur avec # de coloone
+		*/
+
+		RESTORE
+		br		x30					// Retour a l'appelant
+
+/********************************************************************************
+*  Sous-programme qui verifie l'integralite des blocs du sudoku	             	*
+*  x0: addresse du sudoku                                                       *
+********************************************************************************/
+VerifierBloc:
+		SAVE						// Sauvegarde l'environnement de l'appelant
+
+		mov		x19, x0				// Conserve l'adresse de base du sudoku
+		mov		x28, 9				// Nombre d'element dans un bloc a tenir compte
+		mov 	x25, 0				// Bits pour verification init. a 0
+
+		/* faire allumer/etiendre des bits de x25 */
+
+		/*
+		* si all 1's -> la rangee est valide, passer a la prochaine
+		* sinon dispatch a sous-programme pour message d'erreur avec # de bloc
+		*/
+
+		RESTORE
+		br		x30					// Retour a l'appelant
 
 /* Formats de lecture et d'écriture pour printf et scanf */
 .section ".rodata"
 
-scfmt1:     	.asciz  "%d"
-splitter:   	.asciz	"|-------|-------|-------|\n"
+scfmt1:     .asciz  "%d"
+splitter:   .asciz	"|-------|-------|-------|\n"
 barreVert:	.asciz	"| "
 barreVS:	.asciz  "|\n"
 sautLigne:	.asciz	"\n"
@@ -169,4 +288,4 @@ ptfmt4:		.asciz 	"%d "
 tampon:		.skip 4
 Sudoku: 	.skip 81
 dimensions:	.skip 8
-dataMatrice: 	.skip NBLIGNES * NBCOLS * 4	// Espace requis pour matrice 81 octets
+dataMatrice: .skip NBLIGNES * NBCOLS * 4	// Espace requis pour matrice 81 octets
