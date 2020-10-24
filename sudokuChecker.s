54dc44ba3d7f9c18f3270ab8ecbb5878191c6561
Marc-Olivier Lacoste <marc-olivier.lacoste@usherbrooke.ca>
2020-10-24 14:01:37 -0400

    Fully functionnal

--- sudokuChecker.s
+++ sudokuChecker.s
@@ -23,7 +23,7 @@ Main:
 		bl		AfficherSudoku		//Appelle le sous-programme d'affichage
 
 		mov		x0, x20				//Paramètre: adresse du sudoku
-		bl		VerifierSudoku		//Appelle le sous-programme de vérification
+		bl		VerifierSudoku		//Appelle le sous-programme d'affichage
 
 		mov		x0, #0				//0: tous les tampons
 		bl		fflush				//Vidange des tampons
@@ -57,7 +57,6 @@ LireSudoku10:
 
 		RESTORE						// Ramene l'environnment de l'appelant
 		br		x30					// Retour a l'appelant
-
 
 /********************************************************************************
 *  Sous-programme qui affiche le sudoku colonne par colonne		  			    *
@@ -99,8 +98,8 @@ AfficherSudoku15:
 
 		/* Boucle interne: controle le no de ligne (x20 = i) */
 AfficherSudoku20:
-		mul		x22, x20, x28		// Debut de la rangee = i * nb.colonnes
-		add		x22, x22, x21		// Position de l'element = debut.rangee + j
+		mul		x22, x21, x28		// Debut de la rangee = i * nb.colonnes
+		add		x22, x22, x20		// Position de l'element = debut.rangee + j
 		mul 	x22, x22, x23		// Index = position.elmt * 4 octets par elmt
 
 		adr		x0, ptfmt4			// Param1: adresse du format
@@ -143,133 +142,290 @@ AfficherSudoku30:
 		RESTORE						//Ramène l'environnement de l'appelant
 		br		x30					//Retour à l'appelant
 
-
 /********************************************************************************
 *  Sous-programme qui "dispatch" les etapes de verification du sudoku	        *
 *  x0: addresse du sudoku                                                       *
 ********************************************************************************/
 VerifierSudoku:
-		SAVE						// Sauvegarde l'environnement de l'appelant
+		SAVE
 
-		mov		x19, x0				// Conserve l'adresse du sudoku dans x19
+		mov		x0, x20				//Paramètre: adresse du sudoku
+		bl		VerifierR			//Appelle le sous-programme de vérification
 
-		mov		x0, x19				// Param1: adresse de base du sudoku
-		bl		VerifierR			// Branche au sous-programme de verification des rangees
+		mov		x0, x20				//Paramètre: adresse du sudoku
+		bl		VerifierC			//Appelle le sous-programme de vérification
 
-		mov		x0, x19				// Param1: adresse de base du sudoku
-		bl		VerifierC			// Branche au sous-programme de verification des colonnes
+		mov		x0, x20				//Paramètre: adresse du sudoku
+		bl		VerifierB			//Appelle le sous-programme de vérification
 
-		mov		x0, x19				// Param1: adresse de base du sudoku
-		bl		VerifierB			// Branche au sous-programme de verification des blocs
+		RESTORE
+		br		x30
 
+/********************************************************************************
+* 	Sous-programme qui s'occupe de la verification des rangees					*
+*	x0: adresse du Sudoku														*
+********************************************************************************/
 VerifierR:
+		SAVE
+
 		mov		x19, x0				// Conserve l'adresse du tableau dans x19
-		mov		x20, NBLIGNES		// Nombre de lignes a verifier
+		mov		x20, NBCOLS			// Nombre de colonnes a verifier
+		mov		x24, 10				// Valeur de 10 dans x24
 
 VerifierRr:
-		cbz		x20, VerifierC		// Si on a fini d'evaluer les rangees, branche a VerifierC
+		cbz		x20, VerifierRrr	// Si on a fini d'evaluer les rangees, branche a VerifierC
+
+		sub		x23, x24, x20		// Obtenir le numero de ligne courante
 
 		mov		x0, x19				// Param1: l'adresse de l'element courant
+		mov		x1, x23				// Param2: le numero de ligne courant (message d'erreur)
 		bl		VerifierRangee		// Branche au sous-programme verifiant les rangees
 
 		sub		x20, x20, 1			// Decremente le nombre de rangees a verifier
 		add		x19, x19, 36		// Passe a l'adresse de base de la prochaine ligne (adr += 4*9)
 		b.al	VerifierRr			// Revient a la boucle pour prochaine evaluation
 
+VerifierRrr:
+		RESTORE
+		br		x30
+
+/********************************************************************************
+* 	Sous-programme qui s'occupe de la verification des colonnes					*
+*	x0: adresse du Sudoku														*
+********************************************************************************/
 VerifierC:
+		SAVE
+
 		mov		x19, x0				// Conserve l'adresse du tableau dans x19
 		mov		x21, NBCOLS			// Nombre de colonnes a verifier
+		mov		x24, 10				// Valeur de 10 dans x24
 
 VerifierCc:
-		cbz		x21, VerifierB		// Si on a fini d'evaluer les colonnes, branche a VerifierB
+		cbz		x21, VerifierCcc	// Si on a fini d'evaluer les colonnes, branche a VerifierB
+
+		sub		x23, x24, x21		// Obtenir le numero de ligne courante
 
 		mov		x0, x19				// Param1: l'adresse de l'element courant
+		mov		x1, x23				// Param2: le numero de ligne courant (message d'erreur)
 		bl		VerifierColonne		// Branche au sous-programme verifiant les colonnes
 
 		sub		x21, x21, 1			// Decrement le nombre de colonnes a verifier
 		add		x19, x19, 4			// Passe l'adresse de base de la prochaine colonne (adr += 4)
 		b.al	VerifierCc			// Revient a la boucle pour prochaine evalutation
 
+VerifierCcc:
+		RESTORE
+		br		x30
+
+/********************************************************************************
+* 	Sous-programme qui s'occupe de la verification des blocs					*
+*	x0: adresse du Sudoku														*
+********************************************************************************/
 VerifierB:
+		SAVE
+
 		mov		x19, x0				// Conserve l'adresse du tableau dans x19
 		mov		x22, NBBLOCS		// Nombre de blocs a verifier
+		mov		x24, 10				// Valeur de 10 dans x24
+		mov		x20, 1				// Compteur pour skips
 
 VerifierBb:
-		cbz		x22, VerifierF		// Si on a fini d'evaluer les blocs, branche a VerifierF
+		cbz		x22, VerifierBbb	// Si on a fini d'evaluer les blocs, branche a VerifierF
+
+		sub		x23, x24, x22		// Obtenir le numero de ligne courante
 
 		mov		x0, x19				// Param1: l'adresse de l'element courant
+		mov		x1, x23				// Param2: le numero du bloc courant (message d'erreur)
 		bl		VerifierBloc		// Branche au sous-programme verifiant les blocs
 
+		mov		x0, x20				// Param1: n
+		mov		x1, 3				// Param2: d
+		bl		Modulo				// Appel de la fonction Modulo
+		cmp		x0, 99				// Regarde si on a un reste = 0
+		b.ne	VerifierBbNoSkip	// Si reste != 0, alors on doit seulement avancer de 12
+		b.eq	VerifierBbSkip		// Si reste == 0, alors on doit avancer de 84
+
+VerifierBbSkip:
 		sub		x22, x22, 1			// Decremente le nombre de blocs a verifier
-		add		x19, x19, 12		// Passe a l'adresse de base du prochain bloc (adr += 4*3)
+		add		x19, x19, 84		// Passe a l'adresse de base du prochain bloc (adr += 4*21)
+		add		x20, x20, 1			// Incremente le compteur de skips
 		b.al	VerifierBb			// Revient a la boucle pour prochaine evaluation
 
-VerifierF:
+VerifierBbNoSkip:
+		sub		x22, x22, 1			// Decremente le nombre de blocs a verifier
+		add		x19, x19, 12		// Passe a l'adresse de base du prochain bloc (adr += 4*3)
+		add		x20, x20, 1			// Incremente le compteur de skips
+		b.al	VerifierBb			// Revient a la boucle pour prochaine evaluation
+
+VerifierBbb:
 		RESTORE
 		br		x30					//Retour à l'appelant
 
 /********************************************************************************
-*  Sous-programme qui verifie l'integralite des rangees du sudoku	            *
+*  Sous-programme qui verifie l'integralite d'une rangee du sudoku	            *
 *  x0: addresse du sudoku                                                       *
 ********************************************************************************/
 VerifierRangee:
-		SAVE						// Sauvegarde l'environnement de l'appelant
+		SAVE							// Sauvegarde l'environnement de l'appelant
 
-		mov		x19, x0				// Conserve l'adresse de base du sudoku
-		mov		x28, 9				// Nombre d'element dans une rangee a tenir compte
-		mov		x25, 0				// Bits pour verification init. a 0
+		mov		x19, x0					// Conserve l'adresse de base du sudoku
+		mov		x23, x1					// Conserve le # de ligne courante dans x23
+		mov		x28, 9					// Nombre d'element dans une rangee a tenir compte
+		mov		x25, 0					// Bits pour verification init. a 0
+		mov		x26, 1					// Bit pour faire verification avec eor
 
-		/* faire allumer/etiendre des bits de x25 */
+VerifierRangee10:
+		cbz		x28, VerifierRangee20	// Comparaison pour savoir si on a fini la rangee
 
-		/*
-		* si all 1's -> la rangee est valide, passer a la prochaine
-		* sinon dispatch a sous-programme pour message d'erreur avec # de rangee
-		*/
+		ldrb	w20, [x19]				// Load la valeur de l'element courant
+		lsl		x27, x26, x20			// Shift a gauche des bits de la valeur recupere
+		eor		x25, x27, x25			// OU exclusif, 1 si 0, 0 si 1
 
+		add		x19, x19, 4				// Incremente l'adresse de la prochaine cellule
+		sub		x28, x28, 1				// Decremente le nombre d'element restant a evaluer
+		b.al	VerifierRangee10		// Retourne dans la boucle
+
+VerifierRangee20:
+		cmp		x25, 0x3FE				// Compare avec "11111111"
+		b.eq	VerifierRangeeFin		// Si = on branche a VerifierRangeeFin
+
+VRerror:
+		adr		x0, textErrror			// Commencement du message d'erreur
+		bl 		printf
+
+		adr		x0, lineError			// Continuation du message d'erreur
+		bl 		printf
+
+		adr		x0, ptfmt5				// Fin du message d'erreur
+		mov		x1, x23					// # de ligne courant
+		bl 		printf
+
+VerifierRangeeFin:
 		RESTORE
-		br		x30					// Retour a l'appelant
+		br		x30						// Retour a l'appelant
 
 /********************************************************************************
-*  Sous-programme qui verifie l'integralite des colonnes du sudoku	            *
+*  Sous-programme qui verifie l'integralite d'une colonne du sudoku	            *
 *  x0: addresse du sudoku                                                       *
 ********************************************************************************/
 VerifierColonne:
-		SAVE						// Sauvegarde l'environnement de l'appelant
+		SAVE							// Sauvegarde l'environnement de l'appelant
 
-		mov		x19, x0				// Conserve l'adresse de base du sudoku
-		mov		x28, 9				// Nombre d'element dans une colonne a tenir compte
-		mov		x25, 0				// Bits pour verification init. a 0
+		mov		x19, x0					// Conserve l'adresse de base du sudoku
+		mov		x23, x1					// Conserve le # de colonne courante dans x23
+		mov		x28, 9					// Nombre d'element dans une colonne a tenir compte
+		mov		x25, 0					// Bits pour verification init. a 0
+		mov		x26, 1					// Bit pour faire verification avec eor
 
-		/* faire allumer/etiendre des bits de x25 */
+VerifierColonne10:
+		cbz		x28, VerifierColonne20	// Comparaison pour savoir si on a fini la rangee
 
-		/*
-		* si all 1's -> la rangee est valide, passer a la prochaine
-		* sinon dispatch a sous-programme pour message d'erreur avec # de coloone
-		*/
+		ldrb	w20, [x19]				// Load la valeur de l'element courant
+		lsl		x27, x26, x20			// Shift a gauche des bits de la valeur recupere
+		eor		x25, x27, x25			// OU exclusif, 1 si 0, 0 si 1
 
+		add		x19, x19, 36			// Incremente l'adresse de la prochaine cellule
+		sub		x28, x28, 1				// Decremente le nombre d'element restant a evaluer
+		b.al	VerifierColonne10		// Retourne dans la boucle
+
+VerifierColonne20:
+		cmp		x25, 0x3FE				// Compare avec "11111111"
+		b.eq	VerifierColonneFin		// Si = on branche a VerifierColonneFin
+
+VCerror:
+		adr		x0, textErrror			// Commencement du message d'erreur
+		bl 		printf
+
+		adr		x0, colError			// Continuation du message d'erreur
+		bl 		printf
+
+		adr		x0, ptfmt5				// Fin du message d'erreur
+		mov		x1, x23					// # de colonne courant
+		bl 		printf
+
+VerifierColonneFin:
 		RESTORE
-		br		x30					// Retour a l'appelant
+		br		x30						// Retour a l'appelant
 
 /********************************************************************************
-*  Sous-programme qui verifie l'integralite des blocs du sudoku	             	*
+*  Sous-programme qui verifie l'integralite d'un bloc du sudoku	             	*
 *  x0: addresse du sudoku                                                       *
 ********************************************************************************/
 VerifierBloc:
-		SAVE						// Sauvegarde l'environnement de l'appelant
+		SAVE							// Sauvegarde l'environnement de l'appelant
 
-		mov		x19, x0				// Conserve l'adresse de base du sudoku
-		mov		x28, 9				// Nombre d'element dans un bloc a tenir compte
-		mov 	x25, 0				// Bits pour verification init. a 0
+		mov		x19, x0					// Conserve l'adresse de base du sudoku
+		mov		x23, x1					// Conserve le # de bloc courant dans x23
+		mov		x28, 9					// Nombre d'element dans un bloc a tenir compte
+		mov 	x25, 0					// Bits pour verification init. a 0
+		mov		x26, 1					// Bit pour faire verification avec eor
 
-		/* faire allumer/etiendre des bits de x25 */
+VerifierBloc10:
+		cbz		x28, VerifierBloc20		// Si on a fini d'evaluer, on branche a VerifierBloc20
 
-		/*
-		* si all 1's -> la rangee est valide, passer a la prochaine
-		* sinon dispatch a sous-programme pour message d'erreur avec # de bloc
-		*/
+		ldrb	w20, [x19]				// Load la valeur de l'element courant
+		lsl		x27, x26, x20			// Shift a gauche des bits de la valeur recupere
+		eor		x25, x27, x25			// OU exclusif, 1 si 0, 0 si 1
+
+		cmp		x28, 7					// Compare si on est rendu a la fin de la 1iere ligne
+		b.eq	VerifierBlocSkip		// Si oui, on branche a VerifierBlocSkip
+		cmp		x28, 4					// Compare si on est rendu a la fin de la 2ieme ligne
+		b.eq	VerifierBlocSkip		// Si oui, on branche a VerifierBlocSkip
+
+		b.al	VerifierBlocNoSkip		// Sinon, on branche a VerifierBlocNoSkip
+
+VerifierBlocNoSkip:
+		sub		x28, x28, 1				// Decremente le nombre d'element restant a evaluer
+		add		x19, x19, 4				// Incremente l'adresse du prochain element
+		b.al	VerifierBloc10			// Revient dans la boucle d'evalution
+
+VerifierBlocSkip:
+		sub		x28, x28, 1				// Decremente le nombre d'element restant a evaluer
+		add		x19, x19, 28			// Incremente l'adresse du prochain element
+		b.al	VerifierBloc10			// Revient dans la boucle d'evalution
+
+VerifierBloc20:
+		cmp		x25, 0x3FE				// Compare avec "1111111110"
+		b.eq	VerifierBlocFin			// Si = on branche a VerifierColonneFin
+
+VBerror:
+		adr		x0, textErrror			// Commencement du message d'erreur
+		bl 		printf
+
+		adr		x0, blocError			// Continuation du message d'erreur
+		bl 		printf
+
+		adr		x0, ptfmt5				// Fin du message d'erreur
+		mov		x1, x23					// # de colonne courant
+		bl 		printf
+
+VerifierBlocFin:
+		RESTORE
+		br		x30						// Retour a l'appelant
+
+/********************************************************************************
+* Sous-programme qui fait le modulo d'un entier -> n/d							*
+* x0: n																			*
+* x1: d																			*
+* Retourne 1 s'il y a un reste, 0 sion											*
+********************************************************************************/
+Modulo:
+		SAVE
+
+		mov		x19, x0					// Recupere la valeur de "n"
+		mov		x20, x1					// Recupere la valeur de "d"
+		mov		x21, 99					// x21 = 1
+		mov		x22, 45
+
+		udiv	x26, x19, x20			// n MOD d
+		mul		x26, x26, x20
+		subs	x26, x19, x26
+
+		cmp		x26, 0					// Regarde si reste = 0
+		csel	x0, x21, x22, eq		// Si oui x0 = 99, sinon x0 = 45
 
 		RESTORE
-		br		x30					// Retour a l'appelant
+		br		x30
 
 /* Formats de lecture et d'écriture pour printf et scanf */
 .section ".rodata"
@@ -280,6 +436,11 @@ barreVert:	.asciz	"| "
 barreVS:	.asciz  "|\n"
 sautLigne:	.asciz	"\n"
 ptfmt4:		.asciz 	"%d "
+ptfmt5:		.asciz	"%d.\n"
+textErrror:	.asciz	"Le sudoku contient une erreur dans "
+lineError:	.asciz	"la ligne #"
+colError:	.asciz	"la colonne #"
+blocError:	.asciz	"le bloc #"
 
 /* Espace réservé pour recevoir le résultat de scanf. */
 .section ".bss"
